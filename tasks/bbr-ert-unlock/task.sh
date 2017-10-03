0017030dcd513c9f4d665ce04ab6648b91f2d549
abmott <adam.mott@csaa.com>
2017-10-03 15:36:12 -0700

    fixed bbr path in task

--- tasks/bbr-ert-unlock/task.sh
+++ tasks/bbr-ert-unlock/task.sh
@@ -2,7 +2,7 @@
 
 . "$(dirname $0)"/../../scripts/export-director-metadata
 
-  ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
+  ./binary/bbr deployment --target "${BOSH_ADDRESS}" \
   --username "${BOSH_CLIENT}" \
   --deployment "${ERT_DEPLOYMENT_NAME}" \
   --ca-cert "${BOSH_CA_CERT_PATH}" \

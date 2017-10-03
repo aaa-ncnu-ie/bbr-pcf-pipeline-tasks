4398643394e952a7c76389efde7cc3a1c9836f90
abmott <adam.mott@csaa.com>
2017-10-03 15:11:43 -0700

    added bbr unlock tasks

---
+++ tasks/bbr-director-unlock/task.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+
+. "$(dirname $0)"/../../scripts/export-director-metadata
+
+om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
+BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
+
+  ../binary/bbr director --host "${BOSH_ADDRESS}" \
+  --username bbr \
+  --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
+  backup-cleanup

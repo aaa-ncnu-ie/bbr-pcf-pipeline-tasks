15ba16ce98a8bcbc55222f4ddfb051bf7d8b5cc2
abmott <adam.mott@csaa.com>
2017-10-03 11:05:41 -0700

    removed tag from config

--- tasks/export-om-installation/task.sh
+++ tasks/export-om-installation/task.sh
@@ -2,4 +2,6 @@
 
 . $(dirname $0)/../../scripts/om-cmd
 
+om_cmd --request-timeout 7200 delete-unused-products
+
 om_cmd --request-timeout 7200 export-installation --output-file om-installation/installation.zip

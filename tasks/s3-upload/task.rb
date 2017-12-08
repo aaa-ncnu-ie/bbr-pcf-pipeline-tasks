677403cc6c5bd22d981372f63556de1027db996c
abmott <adam.mott@csaa.com>
2017-12-08 12:17:54 -0700

    change s3 upload

---
+++ tasks/s3-upload/task.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'aws-sdk-s3'
+s3 = Aws::S3::Resource.new(
+  :access_key_id => "#{ENV['storage-access-key-id']}",
+  :secret_access_key => "#{ENV['storage-secret-access-key']}",
+  :region => 'us-east-1'
+)
+
+file = "#{ENV['backup_file_name']}"
+bucket = '#{ENV['backup-artifact-bucket']}'
+
+name = File.basename(file)
+
+obj = s3.bucket(bucket).object(name)
+
+obj.upload_file(file)
+
+#File.delete(file)

3b5df86945593a2786f7adf1ae7ee232e7eb8d47
abmott <adam.mott@csaa.com>
2017-12-14 15:47:41 -0700

    added file size

--- tasks/s3-upload/task.rb
+++ tasks/s3-upload/task.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
 require 'aws-sdk-s3'
+
+def filesize(size)
+  units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'Pib', 'EiB']
+
+  return '0.0 B' if size == 0
+  exp = (Math.log(size) / Math.log(1024)).to_i
+  exp = 6 if exp > 6
+
+  '%.1f %s' % [size.to_f / 1024 ** exp, units[exp]]
+end
+
+
 s3 = Aws::S3::Resource.new(
   :access_key_id => "#{ENV['storage-access-key-id']}",
   :secret_access_key => "#{ENV['storage-secret-access-key']}",
@@ -10,7 +22,9 @@ file = "#{ENV['backup_file_name']}"
 bucket = "#{ENV['backup-artifact-bucket']}"
 puts Time.now
 print "uploading file to S3 #{bucket} - "
-puts File.size(file)
+
+upload_size = File.size(file)
+puts filesize(upload_size)
 
 name = File.basename(file)
 
